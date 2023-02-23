4af27c8546d995f67749dfda5289cfb77630b492
maruffahmed <marufah76@gmail.com>
2023-02-24 03:57:36 +0600

    Make eslint happy with PickupListGrid.tsx

--- frontend/app/components/merchant/pickup-list/PickupListGrid.tsx
+++ frontend/app/components/merchant/pickup-list/PickupListGrid.tsx
@@ -14,7 +14,6 @@ import {
 } from '@chakra-ui/react'
 import { BsFillPlusCircleFill } from 'react-icons/bs'
 import { RiPencilLine } from 'react-icons/ri'
-import { useShopProvider } from '~/context/ShopProvider'
 
 function PickupListGrid({
     pickupPoints,
@@ -23,7 +22,6 @@ function PickupListGrid({
     pickupPoints: PickupPoints
     onOpen: () => void
 }) {
-    const { activeShop, chnageActiveShop } = useShopProvider()
     return (
         <SimpleGrid
             columns={{ sm: 2, md: 3, lg: 4 }}
@@ -44,7 +42,7 @@ function PickupListGrid({
                         color="primary.500"
                         fontSize="2xl"
                     />
-                    <Text color="primary.500">Add new shop</Text>
+                    <Text color="primary.500">Add new pickup point</Text>
                 </Flex>
             </Center>
 
@@ -53,20 +51,13 @@ function PickupListGrid({
                       <Card
                           variant="outline"
                           key={pickupPoint.id}
-                          //   borderColor={
-                          //       activeShop?.id === pickupPoint.id
-                          //           ? 'primary.500'
-                          //           : 'gray.200'
-                          //   }
                           cursor="pointer"
-                          //   onClick={() => chnageActiveShop(shop)}
                       >
                           <CardHeader>
                               <Heading size="md"> {pickupPoint.name}</Heading>
                           </CardHeader>
                           <CardBody>
                               <Text>{pickupPoint.address}</Text>
-                              {/* <Text>+8801789393745</Text> */}
                           </CardBody>
                           <CardFooter>
                               <Spacer />
