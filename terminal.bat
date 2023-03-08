f8ecb6c91afb78124dfe1b0bbe44e0fc43fda503
maruffahmed <marufah76@gmail.com>
2023-03-08 22:47:47 +0600

    Remove profile specification

--- terminal.bat
+++ terminal.bat
@@ -1,5 +1,5 @@
-wt nt -p "PowerShell" --title "Server" --tabColor "#ffff00";^
-nt -p "PowerShell" --title "Backend" --tabColor "#dc143c" -d "./backend";^
-nt -p "PowerShell" --title "Frontend" --tabColor "#1e90ff" -d "./frontend";^
-nt -p "PowerShell" --title "Prisma Studio" --tabColor "#ff8c00" -d "./backend";^
-nt -p "PowerShell" --title "Root"
+wt nt --title "Server" --tabColor "#ffff00";^
+nt --title "Backend" --tabColor "#dc143c" -d "./backend";^
+nt --title "Frontend" --tabColor "#1e90ff" -d "./frontend";^
+nt --title "Prisma Studio" --tabColor "#ff8c00" -d "./backend";^
+nt --title "Root"
