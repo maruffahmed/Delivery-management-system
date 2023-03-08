0b930f36704572029cd40d47ff00c91da6c6c278
maruffahmed <marufah76@gmail.com>
2023-03-08 22:40:10 +0600

    Add a batch script for open multiple terminal together

---
+++ terminal.bat
@@ -0,0 +1,5 @@
+wt nt -p "PowerShell" --title "Server" --tabColor "#ffff00";^
+nt -p "PowerShell" --title "Backend" --tabColor "#dc143c" -d "./backend";^
+nt -p "PowerShell" --title "Frontend" --tabColor "#1e90ff" -d "./frontend";^
+nt -p "PowerShell" --title "Prisma Studio" --tabColor "#ff8c00" -d "./backend";^
+nt -p "PowerShell" --title "Root"
