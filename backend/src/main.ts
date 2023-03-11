de77d2d71fe0762e3b3b2c7dc89c87989a408476
maruffahmed <marufah76@gmail.com>
2023-03-11 13:38:50 +0600

    Enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
