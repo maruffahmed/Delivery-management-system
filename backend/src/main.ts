6ed2ce679dc5cce22c151dd6b6d5e3e14e30c67d
maruffahmed <marufah76@gmail.com>
2023-09-17 23:07:05 +0600

    Backend port from env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
+const PORT = process.env.PORT || 5000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
-  await app.listen(8000);
+  await app.listen(PORT);
 }
 bootstrap();
